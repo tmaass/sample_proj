e6b7bbd497f48587cac543b741bf1763fe279054
Trevor Maass <tmaass1@gmail.com>
2012-10-19 13:00:51 -0400

    fourth commit

--- db/migrate/20121019152334_create_images.rb
+++ db/migrate/20121019152334_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
       t.string :url
-      t.string :cat_id
+      t.integer :cat_id
 
       t.timestamps
     end
